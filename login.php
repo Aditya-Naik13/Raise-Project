1766130e11ba71d786d0dc4b3f777bec2ca08245
Aditya <adityanaik1302@gmail.com>
2021-10-19 16:33:20 +0530

    Added many files

---
+++ login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <style>
+      body{
+        background-image:url('login.jpg');
+        backgroud-size:cover;
+      }
+    form{
+      display:flex;
+      flex-direction:column;
+      background-color:white;
+    margin-top: 4em;
+    margin-left: 30em;
+    margin-right:10em;
+    padding: 30px;
+    box-shadow:10px 10px 8px 10px #888888;
+  }
+    </style>
+    <title>Welcome</title>
+</head>
+<body>
+    <div class="container">
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" >
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+    <br>
+  <button type="submit" class="btn btn-primary">Submit</button>
+  <br><br>
+  <div>New User?
+      Register <a href="register.php">here</a>
+  </div>
+</form>
+    </div>
+</body>
+</html>
